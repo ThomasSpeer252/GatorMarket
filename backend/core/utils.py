75cd13f7f7d386f00909d6befdd8cfaa76f2abdf
ThomasSpeer252 <117117392+ThomasSpeer252@users.noreply.github.com>
2025-10-16 11:34:25 -0400

    ORM support
    Added ORM postgresql support. One test script for adding an item to the listing database.

---
+++ backend/core/utils.py
@@ -0,0 +1,17 @@
+from datetime import date
+from core.models import Listing
+
+def addListing(listing_number, title, description, price):
+     #Add a single listing to the database
+
+    date_created = date.today
+
+
+    obj, created = Listing.objects.get_or_create(listing_number=listing_number, defaults={"title": title, "description": description, "date_created": date_created, "price": price}) #Use ORM to create the listing 
+
+    if created:
+        print(f"Listing {listing_number}:{obj.title} inserted in database")#If created, return true. If the listing number already exists or the operation fails, return false
+        return True
+    else:
+        print(f"Listing {listing_number}:{obj.title} failed insertion into the database")
+        return False
